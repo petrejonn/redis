90dec0845ae38a07d5f961e90d59f6ee4c0ef59b
Erimebe <petrejonn@gmail.com>
2024-12-26 05:50:42 +0100

    stage 1 ping command

--- app/server.go
+++ app/server.go
@@ -21,9 +21,20 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+	if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 	}
 }
